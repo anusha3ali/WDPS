bbf9eac81a4faa3de8ad424cbeacef52b5c91da5
Tim <timvankemenade@outlook.com>
2022-12-12 20:42:06 +0100

    Specify UTF-8 encoding to fix UnicodeDecodeError

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import datetime
 import os
 
@@ -22,7 +23,9 @@ def pre_proc_stage(pre_proc_dir, filename):
 
     pre_proc = process_warc_zip()
 
-    save_pre_proc(pre_proc, warc_filename)
+    save_pre_proc(pre_proc_dir, pre_proc, warc_filename)
+
+    return pre_proc
 
 
 def entity_linking_stage():
@@ -34,17 +37,12 @@ def relation_extraction_stage(pre_proc_dir, relation_dir):
     rows = patty.extract_relations_from_zip(f"{pre_proc_dir}/warcs-20221210-141217.csv", with_matcher=True)
     filename = f"{relation_dir}/{current_datetime}"  # Dir seems to be included via save file func TODO centralize this behaviour
     patty.save_file(current_datetime, rows)
-# TODO Trace this error. Occurred in line 8 of warcs-20221210-141217.csv
-# Traceback (most recent call last):
-#   File "C:/Users/Tim/Documents/master-computer_science/WDP/WDPS/main.py", line 72, in <module>
-#     relation_extraction_stage(args.pre_proc_dir, args.relations_dir)
-#   File "C:/Users/Tim/Documents/master-computer_science/WDP/WDPS/main.py", line 34, in relation_extraction_stage
-#     rows = patty.extract_relations_from_zip(f"{pre_proc_dir}/warcs-20221210-141217.csv", with_matcher=True)
-#   File "C:\Users\Tim\Documents\master-computer_science\WDP\WDPS\relation_extraction.py", line 31, in extract_relations_from_zip
-#     for row in csv_reader:
-#   File "C:\Users\Tim\AppData\Local\Programs\Python\Python38\lib\encodings\cp1252.py", line 23, in decode
-#     return codecs.charmap_decode(input,self.errors,decoding_table)[0]
-# UnicodeDecodeError: 'charmap' codec can't decode byte 0x9d in position 2935: character maps to <undefined>
+
+
+def _load_proc_files_from_csv(file_path):
+    with open(file_path, newline='', encoding='UTF-8') as file:
+        csv_reader = csv.reader(file, quoting=csv.QUOTE_NONE, escapechar='\\')
+        return [row for row in csv_reader]
 
 
 if __name__ == "__main__":
@@ -72,14 +70,24 @@ if __name__ == "__main__":
         help="Directory the relations file gets stored.",
         type=str
     )
+    parser.add_argument(
+        "--pre-proc-off",
+        dest="skip_pre_proc",
+        action='store_false',
+        help="Include flag to skip preprocessing."
+    )
     args = parser.parse_args()
 
     # TODO maybe allow turning on/off certain steps
 
     create_dirs(args.pre_proc_dir, args.relations_dir)
 
-    # pre_proc_stage(args.pre_proc_dir, args.pre_proc_filename)
+    if not args.skip_pre_proc:
+        pre_proc_files = pre_proc_stage(args.pre_proc_dir, args.pre_proc_filename)
+    else:
+        pre_proc_files = _load_proc_files_from_csv("pre-proc/warcs-20221210-141217.csv")
 
     entity_linking_stage()
+    print(pre_proc_files[0])
 
     relation_extraction_stage(args.pre_proc_dir, args.relations_dir)
