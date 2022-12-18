eae3697a5aee67bc22a7c5359b7edfb1abcc1ec3
Jeewon Heo <jeewon@Jeewons-MacBook-Pro.local>
2022-12-18 20:50:48 +0100

    changes in EL. Uses livenschetein distance to get most similar string. More query case covergae

--- dbpedia_utils.py
+++ dbpedia_utils.py
@@ -1,40 +1,9 @@
 import ssl
 from SPARQLWrapper import SPARQLWrapper, JSON
+from titlecase import titlecase
+# import urllib
+import time
 
-# TODO: is this only for documentation purposes? I don't see any references to it
-# place = 'Place'
-# person = 'Person'
-# org = 'Organisation'
-# product = 'Work'  # Organisation,,,?
-# work = 'Work'
-# event = 'Event'
-# language = "Language"
-#
-# norp = ["EthnicGroup", "PoliticalParty"]  # low coverage
-# fac = ['Infrastructure', 'Airport', "Building", 'Bridge', "Highway"]  # low coverage
-#
-# groups_dict = {
-#     "Person": ["Person"],
-#     "GPE": ["Location", "Place", "Country", "SpatialThing", "Geo"],
-#     # ? Yago:GeoEntity/Region or geo:SpatialThing (this for all spatial things)
-#     "LOC": ["Location"],
-#     "PRODUCT": ["Work", "Organisation"],
-#     "EVENT": ["Event"],
-#     "FAC": ["Infrastructure", "Airport", "Bridge", "Highway", "Building"],  # ? geo:SpatialThing
-#     "LANGUAGE": ["Language"],
-#     "NORP": ["EthincGroup", "PoliticalParty", "Country"],  # ?
-#     "WORK_OF_ART": ["Work"],
-#     "LAW": [],
-#     "MONEY": ["Currency"],  # ?
-#     "DATE": ["Year", "Month", "Day", "Time"],  # ?
-#     "TIME": ["Time"],  # ?
-#     "CARDINAL": [],  # ?
-#     "ORDINAL": [],  # ?
-#     "PERCENT": []  # ?
-# }
-
-# TODO Okay this is used, but is it necessary to use it as an options list?
-groups = ["dbo:Person", "geo:SpatialThing", "dbo:Organisation", "dbo:Work", "dbo:Event", "dbo:Language"]
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -43,110 +12,148 @@ sparql.setReturnFormat(JSON)
 
 
 def dbpedia_format(mention):
-    mention = mention.title().strip()
-    mention_1 = ' '.join((mention.split()))
-    mention_2 = mention_1.replace(' ', '_')
-    return mention_1, mention_2
+    mention_0 = ' '.join(mention.strip().split())
+    mention_1 = mention_0.replace(' ', '_')
+    mention_2 = titlecase(mention_0)
+    mention_3 = mention_2.replace(' ', '_')
+    return mention_0, mention_1, mention_2, mention_3
 
 
-def build_query(mention, group, query_identifier):
-    mention_1, mention_2 = dbpedia_format(mention)
+def build_query(mentions, group, extra=False):
+    mention_0, mention_1, mention_2, mention_3 = mentions
+    # print(f"({mention_0}) & {mention_1} & ({mention_2}), {mention_3}")
+    # print(group)
 
-    # TODO: this is def bad code, but we need to pick one query and go with it
-    if query_identifier == "pipeline_with_spacy":
+    if extra:
         return f"""
-        PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
-        PREFIX dbpedia: <http://dbpedia.org/>
-        PREFIX dbo:     <http://dbpedia.org/ontology/>
-        SELECT DISTINCT ?item ?name ?page WHERE {{
-            # VALUES ?groups {{dbo:Person dbo:Location}}
-        {{
-            # [Case 1] no disambiguation at all (eg. Twitter)
-            ?item rdfs:label "{mention_1}"@en .
-        }}
+            PREFIX owl:     <http://www.w3.org/2002/07/owl#>
+            PREFIX xsd:     <http://www.w3.org/2001/XMLSchema#>
+            PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
+            PREFIX dc:      <http://purl.org/dc/elements/1.1/>
+            PREFIX dbr:     <http://dbpedia.org/resource/>
+            PREFIX dpr:     <http://dbpedia.org/property/>
+            PREFIX dbpedia: <http://dbpedia.org/>
+            PREFIX skos:    <http://www.w3.org/2004/02/skos/core#>
+            PREFIX dbo:     <http://dbpedia.org/ontology/>
 
-        UNION
-        {{
-            # [Case 2] a dedicated disambiguation page (eg. Michael Jordan)
-            <http://dbpedia.org/resource/{mention_2}_(disambiguation)> dbo:wikiPageDisambiguates ?item.
-        }}
 
-        # Filter by entity class
-        ?item rdf:type {group} .
-        # Grab wikipedia link
-        ?item foaf:isPrimaryTopicOf ?page .
-        # Get name
-        ?item rdfs:label ?name .
-        FILTER (langMatches(lang(?name),"en"))
-        # ?item rdf:type ?group .
-        # ?group rdfs:label ?group_name
-        # FILTER (STR(?group_name) IN ("Building", "Airport"))
-    }}
-    """
-    elif query_identifier == "dbpedia_with_EL" or query_identifier == "popular_entity":
-        return f"""
-        PREFIX owl:     <http://www.w3.org/2002/07/owl#>
-        PREFIX xsd:     <http://www.w3.org/2001/XMLSchema#>
-        PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
-        PREFIX dc:      <http://purl.org/dc/elements/1.1/>
-        PREFIX dbr:     <http://dbpedia.org/resource/>
-        PREFIX dpr:     <http://dbpedia.org/property/>
-        PREFIX dbpedia: <http://dbpedia.org/>
-        PREFIX skos:    <http://www.w3.org/2004/02/skos/core#>
-        PREFIX dbo:     <http://dbpedia.org/ontology/>
-        
-        SELECT DISTINCT ?item ?name ?page WHERE {{
-            # VALUES ?groups {{dbo:Person dbo:Location}}
-        {{
-            # [Case 1] no disambiguation at all (eg. Twitter)
-            ?item rdfs:label "{mention_1}"@en .
+            SELECT DISTINCT ?item ?name ?page WHERE {{
+            {{
+                # [Case 1] no disambiguation at all (eg. Twitter)
+                ?item rdfs:label "{mention_2}"@en .
+            }}
+            UNION
+            {{
+                # [Case 1] lands in a redirect page (eg. "Google, Inc." -> "Google")
+                ?temp rdfs:label "{mention_2}"@en .
+                ?temp dbo:wikiPageRedirects ? ?item .   
+            }}
+            UNION
+            {{
+                # [Case 2] a dedicated disambiguation page (eg. Michael Jordan)
+                <http://dbpedia.org/resource/{mention_3}_(disambiguation)> dbo:wikiPageDisambiguates ?item.
+            }}
+            UNION
+            {{
+                # [Case 3] disambiguation list within entity page (eg. New York)
+                <http://dbpedia.org/resource/{mention_3}> dbo:wikiPageDisambiguates ?item .
+            }}
+            UNION
+            {{
+                # [Case 1] no disambiguation at all (eg. Twitter)
+                ?item rdfs:label "{mention_0}"@en .
+            }}
+            UNION
+            {{
+                # [Case 1] lands in a redirect page (eg. "Google, Inc." -> "Google")
+                ?temp rdfs:label "{mention_0}"@en .
+                ?temp dbo:wikiPageRedirects ? ?item .   
+            }}
+            UNION
+            {{
+                # [Case 2] a dedicated disambiguation page (eg. Michael Jordan)
+                <http://dbpedia.org/resource/{mention_1}_(disambiguation)> dbo:wikiPageDisambiguates ?item.
+            }}
+            UNION
+            {{
+                # [Case 3] disambiguation list within entity page (eg. New York)
+                <http://dbpedia.org/resource/{mention_1}> dbo:wikiPageDisambiguates ?item .
+            }}
+
+            # Filter by entity class
+            ?item rdf:type owl:Thing .
+
+            # Grab wikipedia link
+            ?item foaf:isPrimaryTopicOf ?page .
+
+            # Get name
+            ?item rdfs:label ?name .
+            FILTER (langMatches(lang(?name),"en"))
         }}
-        UNION
-        {{
-            # [Case 1] lands in a redirect page (eg. "Google, Inc." -> "Google")
-            ?temp rdfs:label "{mention_1}"@en .
-            ?temp dbo:wikiPageRedirects ? ?item .   
+        """
+    return f"""
+            PREFIX owl:     <http://www.w3.org/2002/07/owl#>
+            PREFIX xsd:     <http://www.w3.org/2001/XMLSchema#>
+            PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
+            PREFIX dc:      <http://purl.org/dc/elements/1.1/>
+            PREFIX dbr:     <http://dbpedia.org/resource/>
+            PREFIX dpr:     <http://dbpedia.org/property/>
+            PREFIX dbpedia: <http://dbpedia.org/>
+            PREFIX skos:    <http://www.w3.org/2004/02/skos/core#>
+            PREFIX dbo:     <http://dbpedia.org/ontology/>
+
+
+            SELECT DISTINCT ?item ?name ?page WHERE {{
+            {{
+                # [Case 1] no disambiguation at all (eg. Twitter)
+                ?item rdfs:label "{mention_2}"@en .
+            }}
+            UNION
+            {{
+                # [Case 1] lands in a redirect page (eg. "Google, Inc." -> "Google")
+                ?temp rdfs:label "{mention_2}"@en .
+                ?temp dbo:wikiPageRedirects ? ?item .   
+            }}
+            UNION
+            {{
+                # [Case 2] a dedicated disambiguation page (eg. Michael Jordan)
+                <http://dbpedia.org/resource/{mention_3}_(disambiguation)> dbo:wikiPageDisambiguates ?item.
+            }}
+            UNION
+            {{
+                # [Case 3] disambiguation list within entity page (eg. New York)
+                <http://dbpedia.org/resource/{mention_3}> dbo:wikiPageDisambiguates ?item .
+            }}
+
+            # Filter by entity class
+            ?item rdf:type owl:Thing .
+
+            # Grab wikipedia link
+            ?item foaf:isPrimaryTopicOf ?page .
+
+            # Get name
+            ?item rdfs:label ?name .
+            FILTER (langMatches(lang(?name),"en"))
         }}
-        UNION
-        {{
-            # [Case 2] a dedicated disambiguation page (eg. Michael Jordan)
-            <http://dbpedia.org/resource/{mention_2}_(disambiguation)> dbo:wikiPageDisambiguates ?item.
-        }}
-        UNION
-        {{
-            # [Case 3] disambiguation list within entity page (eg. New York)
-            <http://dbpedia.org/resource/{mention_2}> dbo:wikiPageDisambiguates ?item .
-        }}
-        # Filter by entity class
-        ?item rdf:type {group} .
-        
-        # Grab wikipedia link
-        ?item foaf:isPrimaryTopicOf ?page .
-        
-        # Get name
-        ?item rdfs:label ?name .
-        FILTER (langMatches(lang(?name),"en"))
-        # ?item rdf:type ?group .
-        # ?group rdfs:label ?group_name
-        # FILTER (STR(?group_name) IN ("Building", "Airport"))
-    }}
-    """
+        """
 
 
 def generate_candidates(mention, group, query_identifier):
-    query = build_query(mention, group, query_identifier)
+    mentions = dbpedia_format(mention)
+    extra = mentions[0] != mentions[2]
+    query = build_query(mentions, group, extra=extra)
     sparql.setQuery(query)
     sparql.setTimeout(1000)
-    for i in range(2):
+    for _ in range(2):
         try:
             results = sparql.query().convert()
             return results
         except (ConnectionError, TimeoutError):
-            print("Will retry again in a little bit")
+            pass
         except Exception as e:
-            print(e)
+            pass
         time.sleep(15)
--- dbpedia_with_EL.py
+++ dbpedia_with_EL.py
@@ -2,6 +2,8 @@ import ssl
 import requests
 import numpy as np
 import csv
+import time
+from Levenshtein import distance as levenshtein_distance
 
 from dbpedia_utils import generate_candidates
 
@@ -10,64 +12,33 @@ ssl._create_default_https_context = ssl._create_unverified_context
 pruned_groups_dict = {
     "PERSON"        : "dbo:Person",
     "GPE"           : "geo:SpatialThing",
-    "LOC"           : "dbo:Location",
+    "LOC"           : "geo:SpatialThing",
     "FAC"           : "geo:SpatialThing",
-    "ORG"           : "dbo:Organisation",
+    "ORG"           : "owl:Thing",
     "PRODUCT"       : "owl:Thing",
-    "EVENT"         : "dbo:Event",
-    "LANGUAGE"      : "dbo:Language",
+    "EVENT"         : "owl:Thing",
+    "LANGUAGE"      : "owl:Thing",
     "DATE"          : "owl:Thing",
     "NORP"          : "owl:Thing",
-    "WORK_OF_ART"   : "dbo:Work"
+    "WORK_OF_ART"   : "owl:Thing",
 }
 
 
-def levenshtein_distance(s1, s2):
-    # base case: if either string is empty, the distance is the length of the other string
-    if len(s1) == 0:
-        return len(s2)
-    if len(s2) == 0:
-        return len(s1)
-
-    # initialize a matrix to store the distances between substrings
-    distances = [[0 for j in range(len(s2) + 1)] for i in range(len(s1) + 1)]
-
-    # set the distance between the empty string and each substring of s2 to be the index of the substring
-    for i in range(len(s2) + 1):
-        distances[0][i] = i
-
-    # set the distance between each substring of s1 and the empty string to be the index of the substring
-    for i in range(len(s1) + 1):
-        distances[i][0] = i
-
-    # compute the distance between each substring of s1 and each substring of s2
-    for i in range(1, len(s1) + 1):
-        for j in range(1, len(s2) + 1):
-            # if the characters at the current indices are the same,
-            # the distance is the same as the distance between the substrings without those characters
-            if s1[i - 1] == s2[j - 1]:
-                distances[i][j] = distances[i - 1][j - 1]
-            else:
-                # otherwise, the distance is the minimum of the distances
-                # obtained by inserting, deleting, or substituting a character
-                distances[i][j] = min(distances[i][j - 1] + 1, distances[i - 1][j] + 1, distances[i - 1][j - 1] + 1)
-
-    return distances[len(s1)][len(s2)]
-
-
-def get_most_popular_pages(results):
-    if results is None:
-        return None
-
+def get_most_popular_pages(mention, candidates):
     max_backlinks_len = 0
     popular_pages = []
     session = requests.Session()
     url = "https://en.wikipedia.org/w/api.php"
 
-    for result in results["results"]["bindings"]:
-        name = result["page"]
-        x = name["value"].split("/")
-        name = x[-1]
+    if candidates is None or len(candidates["results"]["bindings"]) == 0:
+        return None
+    
+    if len(candidates) == 1:
+        entity_name = candidates[0]["name"]["value"] if "value" in candidates[0]["name"] else candidates[0]["name"]
+        return [(entity_name, candidates[0]["page"]["value"], candidates[0]["item"]["value"])]
+
+    for candidate in candidates["results"]["bindings"]:
+        name = candidate["page"]["value"].split("/")[-1]
         params = {
             "action"        : "query",
             "format"        : "json",
@@ -79,49 +50,106 @@ def get_most_popular_pages(results):
         }
 
         response = session.get(url=url, params=params)
+        if not response:
+            time.sleep(5)
+            response = session.get(url=url, params=params)
+            if not response:
+                continue
         json_data = response.json()
         if "query" in json_data:
             backlinks = json_data["query"]["backlinks"]
             backlinks_len = len(backlinks)
             if backlinks_len >= max_backlinks_len:
                 max_backlinks_len = backlinks_len
-                popular_pages.append((result["name"], result["page"]["value"]))
+                entity_name = candidate["name"]["value"] if "value" in candidate["name"] else candidate["name"]
+                popular_pages.append((entity_name, candidate["page"]["value"], candidate["item"]["value"], backlinks_len))
     
-    return popular_pages
+    if len(popular_pages) > 0:
+        most_popular_pages = [page for page in popular_pages if page[-1] == max_backlinks_len]
+        distances = [levenshtein_distance(mention, page[0]) for page in most_popular_pages]
+        best = np.argmin(distances)
+        return most_popular_pages[best]
+
+    # if len(most_popular_pages) > 0:
+    #     return most_popular_pages
+    
+    distances = []
+    for candidate in candidates["results"]["bindings"]:
+        distance = levenshtein_distance(mention, entity_name)
+        if distance == 0:
+            return (entity_name, candidate["page"]["value"], candidate["item"]["value"])
+        distances.append(distance)
+    best = np.argmin(distances)
+    candidate = candidates[best]
+    entity_name = candidate["name"]["value"] if "value" in candidate["name"] else candidate["name"]
+    return (entity_name, candidate["page"]["value"], candidate["item"]["value"])
 
 
-def get_most_similar_entity(mention, popular_pages):
-    if len(popular_pages) == 0:
+
+def get_most_similar_entity(mention, pages):
+    if not pages or len(pages) == 0:
         return None
 
-    distances = [levenshtein_distance(mention, page[0]) for page in popular_pages]
-    best = np.argmax(distances)
-    return popular_pages[best][1]
+    if len(pages) == 1:
+        return pages[0]
+
+    # distances = [levenshtein_distance(mention, page[0]) for page in pages]
+    distances = []
+    for page in pages:
+        distance = levenshtein_distance(mention, page[0]) 
+        if distance == 0:
+            return page
+        distances.append(distance)
+    best = np.argmin(distances)
+    return pages[best]
 
 
 def get_wikipedia_entity(nlp):
     global_mention_entity = {}
+    rows = []
+    unliked_mentions = 0
+    total_mentions = 0
+    total_documents = 0
+    
+    with open('popular_page_db.csv', 'w', newline='', encoding='UTF-8') as file:
+        writer_db = csv.writer(file, delimiter='\t')
+        with open('popular_page_wiki.csv', 'w', newline='', encoding='UTF-8') as file:
+            writer = csv.writer(file, delimiter='\t')
+            with open("./pre-proc/warcs-20221207-182114.csv", newline='', encoding='cp850') as file:
+                csv_reader = csv.reader(file, quoting=csv.QUOTE_NONE, escapechar='\\')
+                for row in csv_reader:
+                    text = row[-1]
+                    ents = nlp(text).ents
+                    local_mention_entity = {}
+                    total_documents += 1
+                    total_mentions += len(ents)
+                    print(total_documents)
+                    for ent in ents:
+                        mention = ent.text
+                        group = ent.label_
+                        if group in pruned_groups_dict:
+                            if mention not in global_mention_entity:
+                                candidates = generate_candidates(mention, pruned_groups_dict[group], "dbpedia_with_EL")
+                                selected_entity = get_most_popular_pages(mention, candidates)
+                                # selected_entity = get_most_similar_entity(mention, popular_pages)
+                                if selected_entity:
+                                    global_mention_entity[mention] = selected_entity[1], selected_entity[2]
+                                    local_mention_entity[mention] = selected_entity[1], selected_entity[2]
+                            elif global_mention_entity[mention]:
+                                local_mention_entity[mention] = global_mention_entity[mention]
+                            else:
+                                unliked_mentions += 1
 
-    with open('popular_page2.csv', 'w', newline='', encoding='UTF-8') as file:
-        writer = csv.writer(file)
-        with open("warcs-20221207-182114.csv", newline='', encoding='cp850') as file:
-            csv_reader = csv.reader(file, quoting=csv.QUOTE_NONE, escapechar='\\')
-            for row in csv_reader:
-                document = row[-1]
-                doc = nlp(document)
-                local_mention_entity = {}
-                for ent in doc.ents:
-                    mention = ent.text
-                    group = ent.label_
-                    if group in pruned_groups_dict:
-                        if mention not in global_mention_entity:
-                            candidates = generate_candidates(mention, pruned_groups_dict[group], "dbpedia_with_EL")
-                            popular_pages = get_most_popular_pages(candidates)
-                            link = get_most_similar_entity(mention, popular_pages)
-                            global_mention_entity[mention] = link
-                            local_mention_entity[mention] = link
-                        elif global_mention_entity[mention]:
-                            local_mention_entity[mention] = link
+                    if len(local_mention_entity) > 0:
+                        rows = [[f"ENTITY: {row[0]}", mention, link[0]] for mention, link in local_mention_entity.items()]
+                        writer.writerows(rows)
+                        writer_db.writerows(rows)
+                       
+    print(f"DONE, {unliked_mentions} unliked mentions out of {total_documents} documents and {total_mentions} mentions.")
 
-                if len(local_mention_entity) > 0:
-                    writer.writerow([row[0], local_mention_entity])
+
+import spacy, spacy_transformers
+nlp_model = spacy.load("en_core_web_trf", disable=["textcat"])
+start = time.time()
+get_wikipedia_entity(nlp_model)
+print(time.time()-start)
