2baf65726ef05a576adbdf64af5559aa92d30070
Tim <timvankemenade@outlook.com>
2022-12-21 23:24:59 +0100

    Remove unused files

--- run_example.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Processing webpages ..."
-python3 starter_code.py data/warcs/sample.warc.gz > sample_predictions.tsv
-echo "Computing the scores ..."
-python3 score.py data/gold_annotations.tsv sample_predictions.tsv "RELATION"
